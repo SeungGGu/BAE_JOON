eee6bb8eaf775fb07218352a04632b1e5783d9a2
seungbin <b0321ok@gmail.com>
2024-05-28 02:03:37 +0900

    11022 complete

--- src/반복문/bae11022.java
+++ src/반복문/bae11022.java
@@ -1,3 +1,25 @@
+package 반복문;
+
+import java.util.Scanner;
+
 public class bae11022 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int line = 0;
+        line = sc.nextInt();
+
+        int a[] = new int[line];
+        int b[] = new int[line];
+        int[] sum = new int[line];
+
+        for(int i=0; i < line; i++){
+            a[i] = sc.nextInt();
+            b[i] = sc.nextInt();
+            sum[i] = a[i] + b[i];
+        }
+
+        for(int i=0; i<line; i++){
+            System.out.println("Case #" + (i+1) +": " + a[i] + " + " + b[i] + " = " + sum[i]);
+        }
+    }
 }
