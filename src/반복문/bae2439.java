c1842a54093e95079d39f6e9f640c7340b926477
SEUNGBIN PARK <b0321ok@gmail.com>
2024-05-28 22:38:33 +0900

    bae2439

---
+++ src/반복문/bae2439.java
@@ -0,0 +1,22 @@
+package 반복문;
+
+import java.util.Scanner;
+
+public class bae2439 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int line = sc.nextInt();
+        sc.close();
+
+        for (int i = 1; i <= line; i++) {
+            for (int j = 1; j <= line - i; j++) {
+                System.out.print(" ");
+            }
+            for (int l = 0; l < i; l++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
