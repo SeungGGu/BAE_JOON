0f413c49f8208bf9035c5ad60bf04f5bf7d739e6
Seung Bin <b0321ok@gmail.com>
2024-10-21 19:49:29 +0900

    SwEA add

---
+++ src/SwEA/D1/sw2071.java
@@ -0,0 +1,15 @@
+package SwEA.D1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class sw2071 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(bufferedReader.readLine());
+
+
+    }
+}
